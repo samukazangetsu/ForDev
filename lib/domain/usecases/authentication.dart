4e875eefd18cb06ac9b060ffbb243e05f36c9d64
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2021-08-13 15:27:08 -0300

    Alterando Authentication UseCase para receber uma Model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -4,7 +4,16 @@ import '../entities/account_entity.dart';
 
 abstract class Authentication {
   Future<AccountEntity> auth({
-    @required String email,
-    @required String password,
+    @required AuthenticationParams params,
+  });
+}
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({
+    @required this.email,
+    @required this.password,
   });
 }
