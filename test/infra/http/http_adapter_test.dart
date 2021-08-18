370553e60e30ae5cee36e55e805c728421586af9
Samuel Coutinho <samuel_alves_coutinho@hotmail.com>
2021-08-18 09:36:58 -0300

    teste com retorno 200 sem data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -30,7 +30,7 @@ class HttpAdapter implements HttpClient {
       headers: headers,
       body: jsonParsedBody,
     );
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -103,6 +103,20 @@ void main() {
 
           //act
           expect(response, {'any_key': 'any_value'});
+        },
+      );
+
+      test(
+        'Should return null if post resturns 200 without data',
+        () async {
+          //arange
+          when(client.post(any, headers: anyNamed('headers')))
+              .thenAnswer((_) async => Response('', 200));
+          //assert
+          final response = await sut.request(url: url, method: 'post');
+
+          //act
+          expect(response, null);
         },
       );
     },
